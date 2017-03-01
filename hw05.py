4f444ec61b199c7720f5784d380eeb6c26da2aae
Nate Chrisman <nachrism@asu.edu>
2017-02-28 21:40:34 -0700

    Homework #5 Collaboration

---
+++ hw05.py
@@ -0,0 +1,7 @@
+#Orbit of Uranus - homework #5
+#Author: Nate Chrisman / Kelsie Crawford / Brian Pickens
+#Date: March 4, 2017
+
+import numpy as np
+import matplotlib.pyplot as plt
+import outerplanets
